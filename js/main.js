e01c3beaf430d1934eec62da1286f3a569d1f88c
ovezgeldiyev <ovezgeldiyevagabay2000@gmail.com>
2024-06-13 11:54:11 +0500

    asd

---
+++ js/main.js
@@ -0,0 +1,41 @@
+// faq start
+const tabBtn = document.querySelectorAll(".tabBtn");
+const tabEvent = document.querySelectorAll(".tabEvent");
+tabBtn.forEach((e) => {
+  onTabClick(tabBtn, tabEvent, e);
+});
+function onTabClick(tabBtns, tabItems, item) {
+  item.addEventListener("click", function (e) {
+    let currentBtn = item;
+    let tabId = currentBtn.getAttribute("data-tab");
+    let currentTab = document.querySelector(tabId);
+    if (currentBtn.classList.contains("active")) {
+      const faq = currentBtn.parentElement.querySelector(".tabEvent");
+      if (faq) {
+        faq.classList.remove("active");
+        currentBtn.classList.remove("active");
+      }
+    } else if (!currentBtn.classList.contains("active")) {
+      tabBtns.forEach(function (item) {
+        item.classList.remove("active");
+      });
+
+      tabItems.forEach(function (item) {
+        item.classList.remove("active");
+      });
+      currentBtn.classList.add("active");
+      currentTab.classList.add("active");
+    }
+  });
+}
+// faq end
+const contactContent = document.getElementById("contactContent")
+if(contactContent){
+  const contactBtns = document.querySelectorAll(".hero__inner-contact");
+  contactBtns.forEach((contactBtn) => {
+    contactBtn.onclick = () => {
+      console.log("hey")
+      contactContent.classList.add("active");
+    }
+  })
+}
